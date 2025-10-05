ab59a17be95e2cb83fed7c2603d4f6e63d83b1c3
rushil <rushilshah23@users.noreply.github.com>
2025-10-05 14:55:38 +0530

    Spelling fixed for COOKIE_SAME_SITE to COOKIE_SAMESITE

--- configs/env.py
+++ configs/env.py
@@ -7,5 +7,5 @@ class CommonConfig:
     LOG_LEVEL:str= os.environ['LOG_LEVEL'].upper()
     ALLOWED_ORIGINS:List[str] = os.getenv("ALLOWED_ORIGINS", "").split(",")
     COOKIE_HTTP_ONLY: bool = os.environ['COOKIE_HTTP_ONLY']
-    COOKIE_SAME_SITE:Literal['Strict','Lax','None']  = os.environ['COOKIE_SAME_SITE']
+    COOKIE_SAMESITE:Literal['Strict','Lax','None']  = os.environ['COOKIE_SAMESITE']
     COOKIE_SECURE:bool = os.environ['COOKIE_SECURE']
