339ef422f086406894e0fec54880c586e1aabe78
rushil <rushilshah23@users.noreply.github.com>
2025-10-05 14:51:24 +0530

    Added cookie configs

--- configs/env.py
+++ configs/env.py
@@ -1,8 +1,11 @@
 import os
-from typing import List
+from typing import List, Literal
 
 
 class CommonConfig:
     ENVIRONMENT:str = os.environ['ENVIRONMENT']
     LOG_LEVEL:str= os.environ['LOG_LEVEL'].upper()
     ALLOWED_ORIGINS:List[str] = os.getenv("ALLOWED_ORIGINS", "").split(",")
+    COOKIE_HTTP_ONLY: bool = os.environ['COOKIE_HTTP_ONLY']
+    COOKIE_SAME_SITE:Literal['Strict','Lax','None']  = os.environ['COOKIE_SAME_SITE']
+    COOKIE_SECURE:bool = os.environ['COOKIE_SECURE']
